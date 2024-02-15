5e9529d2d3c9c1137922ce3c13835da1d857239e
Arnoud-Wilson <arnoud608@gmail.com>
2024-02-15 15:18:07 +0100

    added navbar elements

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,7 +5,30 @@ function Navigation() {
 
     return (
         <>
-            <h1>Navigation</h1>
+            <div className="overlay overlay-slide-left" id="overlay">
+                <nav>
+                    <ul>
+                        <li id="nav-1" className="slide-out-1 center">
+                            <a href="./index.html" className="center">Home</a>
+                        </li>
+                        <li id="nav-2" className="slide-out-2 center">
+                            <a href="./about.html" className="center">About</a>
+                        </li>
+                        <li id="nav-3" className="slide-out-3 center">
+                            <a href="https://marjoleinbaas.pixieset.com/" target="_blank" className="center">Skills</a>
+                        </li>
+                        <li id="nav-4" className="slide-out-4 center">
+                            <a href="mailto:marjoleinbaas@hotmail.com" className="center">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="hamburger-menu" id="hamburger-menu">
+                <div className="menu-bar1"></div>
+                <div className="menu-bar2"></div>
+                <div className="menu-bar3"></div>
+            </div>
         </>
     );
 }
